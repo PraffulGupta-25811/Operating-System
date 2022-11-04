81802bacf685d11936bcf7f77379248cba74f3ab
Prafful Gupta <praffulgupta09@gmail.com>
2022-11-04 09:47:54 +0530

    indentation

--- IPC/Shared_Memory.c
+++ IPC/Shared_Memory.c
@@ -13,14 +13,15 @@ int main()
 
     int shmid;
     shmid = shmget((key_t)2345, 1024, 0666|IPC_CREAT);
-
     printf("key of shared memory is %d\n", shmid);
-    shared_memory = shmat(shmid, NULL, 0);
 
+    shared_memory = shmat(shmid, NULL, 0);
     printf("Process attached at %p \n", shared_memory);
+
     printf("enter same data to write to shared memory\n");
     read(0, buff, 100);
     strcpy(shared_memory, buff);
+
     printf("you wrote: %s", (char*)shared_memory);
 
 	return 0;
@@ -45,10 +46,9 @@ int main()
 
     int shmid;
     shmid = shmget((key_t)2345, 1024, 0666|IPC_CREAT);
-
     printf("key of shared memory is %d\n", shmid);
-    shared_memory = shmat(shmid, NULL, 0);
 
+    shared_memory = shmat(shmid, NULL, 0);
     printf("Process attached at %p \n", shared_memory);
 
     printf("data read from shared memory is: %s", (char*)shared_memory);
